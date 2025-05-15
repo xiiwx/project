e350dd7023bf52c8a54054bf7076eb275e182f14
xiiwx <amirutyashev23@gmail.com>
2025-05-15 08:56:56 +0300

    Update filesystem.c

--- filesystem.c
+++ filesystem.c
@@ -6,15 +6,19 @@
 char filesystem[FS_SIZE];
 
 // открыть или создать файл
-FILE* open_file(const char* filename) {
+FILE* open_file(const char* filename) 
+{
     return fopen("filesystem.txt", "a+");
 }
 
 // посмотреть файл
-char* view_file(const char* filename) {
+char* view_file(const char* filename) 
+{
     char* line = strtok(filesystem, "\n");
-    while (line != NULL) {
-        if (strcmp(line, filename) == 0) {
+    while (line != NULL) 
+    {
+        if (strcmp(line, filename) == 0) 
+        {
             return strtok(NULL, "\n");
         }
         line = strtok(NULL, "\n");
@@ -23,15 +27,20 @@ char* view_file(const char* filename) {
 }
 
 // удалить файл
-bool delete_file(const char* filename) {
+bool delete_file(const char* filename) 
+{
     char temp[FS_SIZE] = "";
     char* line = strtok(filesystem, "\n");
     int found = 0;
 
-    while (line != NULL) {
-        if (found || strcmp(line, filename) == 0) {
+    while (line != NULL) 
+    {
+        if (found || strcmp(line, filename) == 0) 
+        {
             found = 1;
-        } else {
+        } 
+        else 
+        {
             strcat(temp, line);
             strcat(temp, "\n");
         }
@@ -43,25 +52,31 @@ bool delete_file(const char* filename) {
 }
 
 // новый файл
-bool create_new_file(const char* filename, const char* content) {
+bool create_new_file(const char* filename, const char* content) 
+{
     snprintf(filesystem + strlen(filesystem), FS_SIZE - strlen(filesystem), "%s\n%s\n", filename, content);
     return true;
 }
 
 // изменить файл
-bool modify_file(const char* filename, const char* new_content) {
+bool modify_file(const char* filename, const char* new_content) 
+{
     char temp[FS_SIZE] = "";
     char* line = strtok(filesystem, "\n");
     int found = 0;
 
-    while (line != NULL) {
-        if (strcmp(line, filename) == 0) {
+    while (line != NULL) 
+    {
+        if (strcmp(line, filename) == 0) 
+        {
             found = 1;
             strcat(temp, line);
             strcat(temp, "\n");
             strcat(temp, new_content);
             strcat(temp, "\n");
-        } else {
+        } 
+        else 
+        {
             strcat(temp, line);
             strcat(temp, "\n");
         }
