05a6f31ff2898ddc008355f77a388c42c863957b
xiiwx <amirutyashev23@gmail.com>
2025-05-15 08:55:03 +0300

    Update filesystem.c

--- filesystem.c
+++ filesystem.c
@@ -5,13 +5,12 @@
 #define FS_SIZE 1024
 char filesystem[FS_SIZE];
 
-
+// открыть или создать файл
 FILE* open_file(const char* filename) {
-    FILE *file = fopen("filesystem.txt", "a+");
-    return file;
+    return fopen("filesystem.txt", "a+");
 }
 
-
+// посмотреть файл
 char* view_file(const char* filename) {
     char* line = strtok(filesystem, "\n");
     while (line != NULL) {
@@ -23,7 +22,7 @@ char* view_file(const char* filename) {
     return NULL;
 }
 
-
+// удалить файл
 bool delete_file(const char* filename) {
     char temp[FS_SIZE] = "";
     char* line = strtok(filesystem, "\n");
@@ -39,6 +38,36 @@ bool delete_file(const char* filename) {
         line = strtok(NULL, "\n");
     }
     strncpy(filesystem, temp, FS_SIZE - 1);
-    filesystem[FS_SIZE - 1] = '\0'; 
-    return found; 
+    filesystem[FS_SIZE - 1] = '\0';
+    return found;
+}
+
+// новый файл
+bool create_new_file(const char* filename, const char* content) {
+    snprintf(filesystem + strlen(filesystem), FS_SIZE - strlen(filesystem), "%s\n%s\n", filename, content);
+    return true;
+}
+
+// изменить файл
+bool modify_file(const char* filename, const char* new_content) {
+    char temp[FS_SIZE] = "";
+    char* line = strtok(filesystem, "\n");
+    int found = 0;
+
+    while (line != NULL) {
+        if (strcmp(line, filename) == 0) {
+            found = 1;
+            strcat(temp, line);
+            strcat(temp, "\n");
+            strcat(temp, new_content);
+            strcat(temp, "\n");
+        } else {
+            strcat(temp, line);
+            strcat(temp, "\n");
+        }
+        line = strtok(NULL, "\n");
+    }
+    strncpy(filesystem, temp, FS_SIZE - 1);
+    filesystem[FS_SIZE - 1] = '\0';
+    return found;
 }
