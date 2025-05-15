568325f8f10689812c99b69c5f4eff6e42de53ef
xiiwx <amirutyashev23@gmail.com>
2025-05-15 09:58:44 +0300

    Update filesystem.h

--- filesystem.h
+++ filesystem.h
@@ -1,1 +1,17 @@
+#ifndef FILESYSTEM_H
+#define FILESYSTEM_H
 
+#include <stdio.h>
+#include <stdbool.h>
+
+FILE* open_file(const char* filename);
+
+char* view_file(const char* filename);
+
+bool delete_file(const char* filename);
+
+bool write_file(const char* filename, const char* data);
+
+bool file_exists(const char* filename);
+
+#endif 
