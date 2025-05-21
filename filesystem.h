3f1026f149d1676edad113acf97563a866414023
xiiwx <amirutyashev23@gmail.com>
2025-05-22 00:37:13 +0300

    Update filesystem.h

--- filesystem.h
+++ filesystem.h
@@ -3,13 +3,13 @@
 
 #include <stdio.h>
 
-FILE* open_fs(const char* filename);
+FILE* open_file(const char* filename);
 
 void view_file(const char* fs_filename, const char* target_filename);
 
 void delete_file(const char* fs_filename, const char* target_filename);
 
-void add_new_file(const char* fs_filename, const char* new_filename, const char* content_lines[], int content_count);
+void new_file(const char* fs_filename, const char* new_filename, const char* content_lines[], int content_count);
 
 void modify_file(const char* fs_filename, const char* target_filename, const char* new_content[], int new_count);
 
